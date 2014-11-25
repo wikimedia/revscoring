97a68a321fc79aa2874404f33870064def4871c4
Helder <he7d3r@gmail.com>
2014-11-24 23:25:18 -0200

    Use \w+ to detect words and add tests

--- revscores/feature_extractors/tests/test_words_added.py
+++ revscores/feature_extractors/tests/test_words_added.py
@@ -7,3 +7,5 @@ from ..words_added import words_added
 
 def test_words_added():
     eq_(words_added(["This is four words.", "And two."]), 6)
+    eq_(words_added(["Só um teste"]), 3)
+    eq_(words_added(["É isso aí", "Temos mais um teste em ação", "Legal, não é?"]), 12)
