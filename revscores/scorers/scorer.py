cc62cdb43996517987bc5a537a9cd78b83e3ef58
halfak <aaron.halfaker@gmail.com>
2014-12-14 12:50:19 -0600

    Adds abstract interface for scorer.

--- revscores/scorers/scorer.py
+++ revscores/scorers/scorer.py
@@ -1,38 +1,94 @@
 
 
 class Scorer:
+    """
+    Interface for implementing a wide variety of scoring strategies.
+    """
     
-    FEATURES = []
-    
-    def __init__(self, feature_extractor):
-        self.feature_extractor = feature_extractor
+    def __init__(self, source):
+        self.source = source
     
     def score(self, rev_ids):
-        feature_sets = (self.feature_extractor.extract(rev_id, self.FEATURES)
-                        for rev_id in rev_ids)
+        """
+        Returns a sequence of scores that correspond to <rev_ids>.
+        """
+        raise NotImplementedError()
+    
+ 
+class MLScorer(Scorer):
+    """
+    Machine Learning Scorer -- a type of Scorer design to support a machine
+    learned model of some sort.
+    """
+    
+    MODEL = NotImplemented
+    
+    def __init__(self, source, model):
+        """
+        Constructs a new scorer with a given model.  Note this scorer expects
+        the model to already be trained.
+        """
+        super().__init__(source)
         
-        return self._score(feature_sets)
+        assert isinstance(model, self.MODEL)
+        self.model = model
     
-    def _score(self, feature_set): pass
-    
-    def _validate_features(self, feature_set):
-        return [extractor.return_type(feature)
-                for extractor, feature in zip(self.FEATURES, feature_set)]
-    
-class ModelScorer(Scorer):
-    
-    def train(self, rev_ids, scores):
-        feature_sets = (self.feature_extractor.extract(rev_id, self.FEATURES)
-                        for rev_id in rev_ids)
+    def score(self, rev_ids):
+        values = self.extract(rev_ids)
         
-        return self._train(feature_sets, scores)
+        scores = self.model.score(values)
         
-    def _train(self, feature_sets, scores): pass
+        return scores
     
-    def test(self, rev_ids, scores):
-        feature_sets = (self.feature_extractor.extract(rev_id, self.FEATURES)
-                        for rev_id in rev_ids)
+    def extract(self, rev_ids):
+        """
+        Extracts the model's features for a set of rev_ids.
+        """
+        return (self.source.extract(rev_id, self.model.features)
+                for rev_id in rev_ids)
         
-        return self._test(feature_sets, scores)
     
-    def _test(self, feature_sets, scores): pass
+
+class MLScorerModel:
+    """
+    A machine learned model to be used by a MLScorer
+    """
+    
+    def __init__(self, features):
+        """
+        Constructs a new Machine Learned scoring model.
+        
+        :Parameters:
+            extractors : `list`(`feature extractor`)
+                A list of extractors that will be used to train
+        """
+        self.features = tuple(features)
+    
+    
+    def train(self, values_scores):
+        """
+        Trains the model on an iterable of labeled data (<features>, <score>).
+        """
+        raise NotImplementedError()
+        
+    
+    def test(self, values_scores):
+        """
+        Returns a dictionary of test results based on an iterable of labeled data (<features>, <score>)
+        """
+        raise NotImplementedError()
+    
+    
+    def score(self, values):
+        """
+        Returns a prediction based on a set of features.
+        """
+        raise NotImplementedError()
+    
+    
+    def _validate_features(self, values):
+        """
+        Checks the features against provided values to confirm types, ordinality, etc.
+        """
+        return [feature.return_type(value)
+                for feature, value in zip(self.feature, values)]
