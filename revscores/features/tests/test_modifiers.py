a023b3abc667b4b8ebe0a8ecb0127ca80111ecab
halfak <aaron.halfaker@gmail.com>
2015-01-18 16:13:19 -0600

    Fixed dependency issues with modifiers.

--- revscores/features/tests/test_modifiers.py
+++ revscores/features/tests/test_modifiers.py
@@ -3,6 +3,7 @@ from math import log
 from nose.tools import eq_
 
 from .. import modifiers
+from ...dependent import solve
 from ..feature import Feature
 
 
@@ -14,7 +15,7 @@ def test_log():
     
     log_five = modifiers.log(five)
     
-    eq_(log_five(), log(5))
+    eq_(solve(log_five), log(5))
 
 
 def test_sum_sub():
@@ -25,9 +26,9 @@ def test_sum_sub():
     
     five_plus_one = modifiers.add(five, 1)
     
-    eq_(five_plus_one(), 6)
+    eq_(solve(five_plus_one), 6)
     
     five_minus_one = modifiers.sub(five, 1)
     
     print(five_minus_one)
-    eq_(five_minus_one(), 4)
+    eq_(solve(five_minus_one), 4)
