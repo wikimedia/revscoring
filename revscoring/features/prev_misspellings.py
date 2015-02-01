eb9145dd0ac8a365fbfeb228a7b6376c524f3af3
Helder <he7d3r@gmail.com>
2015-02-01 10:48:38 -0200

    Use previous revision text to count previous (bad)words and misspellings
    Fixes issue #37.

--- revscoring/features/prev_misspellings.py
+++ revscoring/features/prev_misspellings.py
@@ -1,6 +1,6 @@
 import re
 
-from ..datasources import revision_text
+from ..datasources import previous_revision_text
 from .feature import Feature
 
 WORD_RE = re.compile('\w+', re.UNICODE)
@@ -9,13 +9,13 @@ WORD_RE = re.compile('\w+', re.UNICODE)
 def process(language, revision_text):
     revision_text = revision_text or ''
     misspellings = 0
-    
+
     words = (m.group(0) for m in WORD_RE.finditer(revision_text))
     for misspelling in language.misspellings(words):
         misspellings += 1
-        
+
     return misspellings
 
 prev_misspellings = Feature("prev_misspellings", process,
                             returns=int,
-                            depends_on=["language", revision_text])
+                            depends_on=["language", previous_revision_text])
