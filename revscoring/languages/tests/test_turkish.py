59ed4393bd6fa784d352ddafd22dddc7201268fb
ToAruShiroiNeko <to.aru.shiroi.neko@gmail.com>
2015-02-01 19:38:17 +0100

    fixing spacing

--- revscoring/languages/tests/test_turkish.py
+++ revscoring/languages/tests/test_turkish.py
@@ -6,14 +6,14 @@ from ..turkish import turkish
 def test_language():
     
     assert turkish.is_badword("mal")
-    assert turkish.is_badword("shitty")
-    assert not turkish.is_badword("hat")
+    assert turkish.is_badword("orospu çocuğu")
+    assert not turkish.is_badword("resim")
     
     assert turkish.is_misspelled("wjwkjb")
-    assert not turkish.is_misspelled("waffles")
+    assert not turkish.is_misspelled("sinema")
     
-    eq_(list(turkish.badwords(["good", "slut", "bad", "ugly", "fucker"])),
-        ["slut", "fucker"])
+    eq_(list(turkish.badwords(["komedi", "pezevenk", "kedi", "güzergah", "ibne"])),
+        ["pezevenk", "ibne"])
     
-    eq_(list(turkish.misspellings(["waffles", "oof", "dog", "blarg"])),
+    eq_(list(turkish.misspellings(["sinema", "oof", "köpek", "blarg"])),
         ["oof", "blarg"])
