820dff9ef2178f7dd46735a8c2250f4c0a49c51b
halfak <aaron.halfaker@gmail.com>
2015-02-01 18:30:03 -0600

    Adds div and mul to Feature as well as tests.

--- revscoring/features/tests/test_feature.py
+++ revscoring/features/tests/test_feature.py
@@ -7,36 +7,50 @@ from .. import modifiers
 from ...dependent import solve
 from ..feature import Feature
 
+five = Feature("five", lambda: 5, returns=int, depends_on=[])
 
-def return_foo(foo):
-    return foo
+def identity_process(value): return value
+int_identity = Feature("int_identity", identity_process,
+                                       returns=int, depends_on=["value"])
 
 def test_feature():
     
-    foobar = Feature("foobar", return_foo,
-                     returns=int, depends_on=["foo"])
+    eq_(int_identity(5), 5)
     
-    eq_(foobar(5), 5)
-    
-    eq_(pickle.loads(pickle.dumps(foobar))(5), 5)
+    eq_(pickle.loads(pickle.dumps(int_identity))(5), 5)
 
 @raises(ValueError)
 def test_feature_type():
     
-    foobar = Feature("foobar", return_foo, returns=int, depends_on=["foo"])
+    int_identity(11)
     
-    foobar(11)
-    
-    foobar("not int")
+    int_identity("not int")
 
 def test_add_sub():
     
-    five = Feature("five", lambda: 5, returns=int, depends_on=[])
-    
     five_plus_one = five + 1
+    five_minus_one = five - 1
+    five_plus_five = five + five
+    five_minus_five = five - five
     
     eq_(five_plus_one.returns, int)
     eq_(solve(five_plus_one), 6)
+    eq_(solve(five_minus_one), 4)
+    eq_(solve(five_plus_five), 10)
+    eq_(solve(five_minus_five), 0)
+
+def test_mul_div():
+    
+    five_times_two = five * 2
+    five_divide_two = five / 2
+    five_times_five = five * five
+    five_divide_five = five / five
+    
+    eq_(five_times_two.returns, int)
+    eq_(solve(five_times_two), 10)
+    eq_(solve(five_divide_two), 2.5)
+    eq_(solve(five_times_five), 25)
+    eq_(solve(five_divide_five), 1)
 
 def test_log():
     
@@ -46,15 +60,3 @@ def test_log():
     
     eq_(log_five_plus_one.returns, float)
     eq_(solve(log_five_plus_one), math_log(5+1))
-
-def test_solve():
-    
-    five = Feature("five", lambda: 5, returns=int, depends_on=[])
-    
-    five_plus_one = Feature("five_plus_one", lambda five: five+1,
-                            returns=int, depends_on=[five])
-    
-    modified_five_plus_one = five + 1
-    
-    eq_(solve(five_plus_one), 6)
-    eq_(solve(modified_five_plus_one), 6)
