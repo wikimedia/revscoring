ec6d60f41d84b12f75cb46b482bb8b4ec29658e1
ToAruShiroiNeko <toarushiroineko@gmail.com>
2015-02-28 20:19:34 +0100

    Adding Azerbaijani language

--- revscoring/languages/__init__.py
+++ revscoring/languages/__init__.py
@@ -2,3 +2,5 @@ from .language import Language, LanguageUtility
 from .language import stem_word, is_badword, is_misspelled, is_stopword
 from .english import english
 from .portuguese import portuguese
+from .turkish import turkish
+from .azerbaijani import azerbaijani
