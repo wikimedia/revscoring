725297b0cf0dd0cbff033bcfef760810b18d00cd
halfak <aaron.halfaker@gmail.com>
2015-03-09 11:52:55 -0500

    Updated for refactor of deltas

--- revscoring/datasources/diff.py
+++ revscoring/datasources/diff.py
@@ -1,5 +1,5 @@
 from deltas import segment_matcher
-from deltas.tokenizers import WikitextSplit
+from deltas.tokenizers import wikitext_split
 
 from . import parent_revision, revision
 from .datasource import Datasource
@@ -9,21 +9,19 @@ from .util import WORD_RE
 def process_operations(parent_revision_text, revision_text):
     parent_revision_text = parent_revision_text or ''
     revision_text = revision_text or ''
-    
-    tokenizer = WikitextSplit()
-    
-    a = tokenizer.tokenize(parent_revision_text)
-    b = tokenizer.tokenize(revision_text)
-    
+
+    a = wikitext_split.tokenize(parent_revision_text)
+    b = wikitext_split.tokenize(revision_text)
+
     return [op for op in segment_matcher.diff(a, b)], a, b
 
 operations = Datasource("diff.operations", process_operations,
                         depends_on=[parent_revision.text, revision.text])
 
 def process_added_tokens(diff_operations):
-    
+
     operations, a, b = diff_operations
-    
+
     return [t for op in operations
             if op.name == "insert"
             for t in b[op.b1:op.b2]]
@@ -32,9 +30,9 @@ added_tokens = Datasource("diff.added_tokens", process_added_tokens,
                           depends_on=[operations])
 
 def process_removed_tokens(diff_operations):
-    
+
     operations, a, b = diff_operations
-    
+
     return [t for op in operations
             if op.name == "delete"
             for t in a[op.a1:op.a2]]
@@ -44,7 +42,7 @@ removed_tokens = Datasource("removed_tokens", process_removed_tokens,
 
 def process_added_segments(diff_operations):
     operations, a, b = diff_operations
-    
+
     return ["".join(b[op.b1:op.b2])
             for op in operations\
             if op.name == "insert"]
@@ -54,7 +52,7 @@ added_segments = Datasource("diff.added_segments", process_added_segments,
 
 def process_removed_segments(revision_diff):
     operations, a, b = revision_diff
-    
+
     return ["".join(a[op.a1:op.a2])
             for op in operations\
             if op.name == "delete"]
@@ -65,13 +63,13 @@ removed_segments = Datasource("diff.removed_segments", process_removed_segments,
 def process_added_words(diff_added_segments):
     return [match.group(0) for segment in diff_added_segments
                            for match in WORD_RE.finditer(segment)]
-               
+
 added_words = Datasource("diff.added_words", process_added_words,
                            depends_on=[added_segments])
-                           
+
 def process_removed_words(diff_removed_segments):
    return [match.group(0) for segment in diff_removed_segments
                           for match in WORD_RE.finditer(segment)]
-              
+
 removed_words = Datasource("diff.removed_words", process_removed_words,
                           depends_on=[removed_segments])
