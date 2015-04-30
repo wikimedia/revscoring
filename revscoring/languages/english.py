9ab3c51c9fe2121db67b2d8e84bde57995baed42
halfak <aaron.halfaker@gmail.com>
2015-04-30 09:47:15 -0500

    Converts english to enchant

--- revscoring/languages/english.py
+++ revscoring/languages/english.py
@@ -1,6 +1,7 @@
 import warnings
 
-from nltk.corpus import stopwords, wordnet
+import enchant
+from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
 from .language import Language, LanguageUtility
@@ -34,7 +35,8 @@ BADWORDS = set([
     "yank", "yankee", "yid",
     "zipperhead"
 ])
-STEMMED_BADWORDS = set(STEMMER.stem(w) for w in BADWORDS)
+STEMMMED_BADWORDS = set(STEMMER.stem(w) for w in BADWORDS)
+DICTIONARY = enchant.Dict("en")
 
 def stem_word_process():
     def stem_word(word):
@@ -51,10 +53,9 @@ is_badword = LanguageUtility("is_badword", is_badword_process, depends_on=[stem_
 
 def is_misspelled_process():
     def is_misspelled(word):
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            return len(wordnet.synsets(word)) == 0
+        return not DICTIONARY.check(word)
     return is_misspelled
+
 is_misspelled = LanguageUtility("is_misspelled", is_misspelled_process,
                                 depends_on=[])
 
