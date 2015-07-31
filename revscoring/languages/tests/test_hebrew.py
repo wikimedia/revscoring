100a8b8288712c7a2bfff62551b09b71edbc0815
eranroz <eranroz89@gmail.com>
2015-07-31 20:25:30 +0300

    Merge

--- revscoring/languages/tests/test_hebrew.py
+++ revscoring/languages/tests/test_hebrew.py
@@ -18,8 +18,6 @@ def test_language():
     is_informal_word = hebrew.solve(language.is_informal_word)
 
     assert is_informal_word("בגללך")
-<<<<<<< HEAD
+
     assert not is_informal_word("בגלל")
-=======
     assert not is_informal_word("בגלל")
->>>>>>> 9a762e70c049be565ca16d3ede133ebd90e6f816
