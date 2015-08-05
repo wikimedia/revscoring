d018fdfe2720512c0101d7534605a92a8a395a28
reza <reza.energy@gamil.com>
2015-08-06 04:09:43 +0430

    pep8

--- revscoring/languages/persian.py
+++ revscoring/languages/persian.py
@@ -23,16 +23,21 @@ INFORMAL_REGEXES = set([
 BAD_REGEXES  = set([
     r"(madar|nanae|zan|khahar)\s*?(ghahbeh|ghahveh|ghabe|jendeh?|be khata)",
     r"khar madar",r"khar kos deh",
-    r"([qkc]+o+s+|[qkc]+o+n+|[qkc]+i+r+|m+e+g+h+a+d)\s*?((va|o)\s*?[qkc]oon|lis|pareh?|k+h+a+r+|[qkc]esh|nane|nanat|babat|khah?a?r|abjit|mi ?dad|mi ?dah[iy]|[qkc]on|deh|khor|goshad|gondeh|[qkc]oloft|[qkc]esh|mashang|khol|baz|shenas|nag[uo]o?|maghz|sh[ae]r)",
-    r"\bmameh?",r"sho+[mn]bo+l",r"\brazl",r"gaei?d[ia]m",r"\bk+i+r+i+",r"\bk+o+s+o+",r"\bk+o+n+i+",
-    r"j+e+n+d+e+h?",r"[qkc]iram",r"(pedar|baba|naneh?|tokhme?) sag",r"pedasag",r"bi (sho+r|shour|sharaf|namo+s)",
-    r"madareto?",r"\bamato?",r"da[iy]o+s",r"goh? ?nakhor",r"\bashghal",r"\bavazi",
-    r"کیرم", r"کونی", r"برووتو", r"لعنت", r"کاکاسیاه", r"آشغال", r"گائیدم", r"گوزیده", r"مشنگ", r"ننتو", r"بخواب",
+    r"([qkc]+o+s+|[qkc]+o+n+|[qkc]+i+r+|m+e+g+h+a+d)\s*?((va|o)\s*?[qkc]oon|"+
+        r"lis|pareh?|k+h+a+r+|[qkc]esh|nane|nanat|babat|khah?a?r|abjit|mi ?dad"+
+        r"|mi ?dah[iy]|[qkc]on|deh|khor|goshad|gondeh|[qkc]oloft|[qkc]esh|mashang"+
+        r"|khol|baz|shenas|nag[uo]o?|maghz|sh[ae]r)",
+    r"\bmameh?",r"sho+[mn]bo+l",r"\brazl",r"gaei?d[ia]m",r"\bk+i+r+i+",r"\bk+o+s+o+",
+    r"\bk+o+n+i+",r"j+e+n+d+e+h?",r"[qkc]iram",r"(pedar|baba|naneh?|tokhme?) sag",
+    r"pedasag",r"bi (sho+r|shour|sharaf|namo+s)",    r"madareto?",r"\bamato?",
+    r"da[iy]o+s",r"goh? ?nakhor",r"\bashghal",r"\bavazi",
+    r"کیرم", r"کونی", r"برووتو", r"لعنت", r"کاکاسیاه", r"آشغال", r"گائیدم", r"گوزیده",
+    r"مشنگ", r"ننتو", r"بخواب",r"خار مادر",r"خوار کس ده",r"شو?مبول",r"\bممه\b",
     r"\b((ما\.?در|ننه|زن|خو?اه?ر) ?(خرابه|ق\.?[حه]\.?ب\.?ه|قحبه|قبه|ج\.?ن\.?د\.?ه|به خطا)",
-    r"خار مادر",r"خوار کس ده",
-    r"\b([کك]+س+|[کك]+و+ن+|[کك]+[یي]+ر+|مقعد|عضو ?تحتانی|ما?تحت)\s*(و کون|لیسی?|پاره|خر|[کك]ش|نن[هت]\b|بابات|خو?اه?ر|آبجیت|هم ?شیره|می ?داد|می ?ده?ی|می ?کنی?|کن|خور)",
-    r"\b[کك]+(و+ن|س)\s*(خر|گشاد|گنده|کش|مشنگ|پاره|ننت|ننه\b|خل|باز|خور\b|شناس|نگو|مغز|ه؟ ؟شعر|و ?شعر|مادر|خو?اه?ر|آبجیت|هم ?شیره|داد)",
-    r"شو?مبول",r"\bممه\b",
+    r"\b([کك]+س+|[کك]+و+ن+|[کك]+[یي]+ر+|مقعد|عضو ?تحتانی|ما?تحت)\s*(و کون|لیسی?|پاره|خر|"+
+        r"[کك]ش|نن[هت]\b|بابات|خو?اه?ر|آبجیت|هم ?شیره|می ?داد|می ?ده?ی|می ?کنی?|کن|خور)",
+    r"\b[کك]+(و+ن|س)\s*(خر|گشاد|گنده|کش|مشنگ|پاره|ننت|ننه\b|خل|باز|خور\b|شناس|نگو|مغز|"+
+        r"ه؟ ؟شعر|و ?شعر|مادر|خو?اه?ر|آبجیت|هم ?شیره|داد)",
     r"ر[زذ]ل\b",r"[کك]+[یي]+ر+\s*م?(ی|خر|(ب|)خور|تو[ی ]|مو |دهن)",
     r"گا[يئی]ید[میي]",r"گاهييد[نه]",r"بگامت\b",r"(پدر|ننه|مادر|بابا|تخم)\s*سگ",
     r"بی ?(شعور|شرف|ناموس)[یي]", r"\پريودى\؟",r"مادرت گا",
