ffb0d0d83494b13211cb4580b8da76a8cf0ef4a0
reza <reza.energy@gamil.com>
2015-08-07 00:58:16 +0430

    undo

--- revscoring/languages/tests/test_persian.py
+++ revscoring/languages/tests/test_persian.py
@@ -5,19 +5,12 @@ from .. import language, persian
 
 def test_language():
 
-    is_badword = persian.solve(language.is_badword)
-
-    assert is_badword("کیرم")
-    assert is_badword("madar ghahbeh")
-    assert not is_badword("hat")
-
-    is_informal_word = english.solve(language.is_informal_word)
-
-    assert is_informal_word("شادروان")
-    assert is_informal_word("خدا بیامرز")
-    assert not is_informal_word("hat")
-
     is_misspelled = persian.solve(language.is_misspelled)
 
     assert is_misspelled("Notafarsiword")
     assert not is_misspelled("مهم")
+
+    is_badword = persian.solve(language.is_badword)
+
+    assert is_badword("کیرم")
+    assert not is_badword("hat")
