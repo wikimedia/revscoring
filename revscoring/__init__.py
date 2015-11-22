2a2497e3ccf7d2caf56b889eec2e14267c143c5c
halfak <aaron.halfaker@gmail.com>
2015-11-22 13:10:42 -0600

    Increments version to 0.7.3

--- revscoring/__init__.py
+++ revscoring/__init__.py
@@ -108,6 +108,6 @@ from .features import Feature
 from .languages import Language
 from .scorer_models import ScorerModel
 
-__version__ = "0.7.2"
+__version__ = "0.7.3"
 
 __all__ = [Datasource, Dependent, Extractor, Feature, Language, ScorerModel]
