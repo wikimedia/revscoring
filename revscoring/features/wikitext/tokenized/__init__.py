809d2308e48a8375b9993902786984958aa678b8
halfak <aaron.halfaker@gmail.com>
2015-12-22 14:04:37 -0600

    Moves tokenized.delta to tokenized.diff.

--- revscoring/features/wikitext/tokenized/__init__.py
+++ revscoring/features/wikitext/tokenized/__init__.py
@@ -2,7 +2,7 @@ from .tokenized import tokenized
 from .tokenized_revision import (TokenizedRevision, TokenIsInTypes,
                                  is_uppercase_word)
 from .revision_oriented import revision
-from . import delta
+from .diff import diff
 
-__all__ = [tokenized, revision, delta,
+__all__ = [tokenized, revision, diff,
            TokenizedRevision, TokenIsInTypes, is_uppercase_word]
