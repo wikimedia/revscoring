b2e86c7ada55013c4d4b5690d39a0b353fbf55b5
halfak <aaron.halfaker@gmail.com>
2016-04-12 14:17:03 -0500

    Increments version to 1.2.0

--- revscoring/__init__.py
+++ revscoring/__init__.py
@@ -116,7 +116,7 @@ from .extractors import Extractor
 from .features import Feature
 from .scorer_models import ScorerModel
 
-__version__ = "1.1.8"  # Change in setup.py
+__version__ = "1.2.0"  # Change in setup.py
 
 __all__ = [Datasource, Dependent, DependentSet, Extractor, Feature,
            ScorerModel]
