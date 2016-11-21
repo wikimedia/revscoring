55f32bdd81aacfde05ccf1ff7b3f009e5378875f
halfak <aaron.halfaker@gmail.com>
2016-11-21 15:27:24 -0600

    Normalizes NaN, Inf, and -Inf in JSON formatting.

--- revscoring/scorer_models/tests/test_util.py
+++ revscoring/scorer_models/tests/test_util.py
@@ -5,10 +5,13 @@ from ..util import balance_sample, balance_sample_weights, normalize_json
 
 
 def test_normalize_json():
-    doc = {"foo": {numpy.bool_(True): "value"}, "what": numpy.bool_(False)}
+    doc = {"foo": {numpy.bool_(True): "value"},
+           "what": numpy.bool_(False),
+           "this": numpy.PINF}
     normalized_doc = normalize_json(doc)
     eq_(type(normalized_doc['what']), bool)
     eq_(type(list(normalized_doc['foo'].keys())[0]), bool)
+    eq_(normalized_doc['this'], "Infinity")
 
 
 def test_balance_sample():
