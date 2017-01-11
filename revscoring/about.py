be6bec347f16c07c4eb876d9b6d4a2f6c1cd77a8
halfak <aaron.halfaker@gmail.com>
2017-01-11 10:26:45 -0800

    Adds about.py file for tracking metadata.

---
+++ revscoring/about.py
@@ -0,0 +1,8 @@
+__name__ = "revscoring"
+__version__ = "1.3.3"
+__author__ = "Aaron Halfaker"
+__author_email__ = "ahalfaker@wikimedia.org"
+__description__ = ("A set of utilities for generating quality scores for " +
+                   "MediaWiki revisions")
+__url__ = "https://github.com/wiki-ai/revscoring"
+__license__ = "MIT"
