431cb985ba83fbf7a42a6773cde1b3cb900be984
halfak <aaron.halfaker@gmail.com>
2017-03-18 12:16:02 -0500

    Bumps deltas requirement to 0.4.5 and fixes a test for gramming.

--- revscoring/datasources/meta/tests/test_gramming.py
+++ revscoring/datasources/meta/tests/test_gramming.py
@@ -12,8 +12,8 @@ my_grams = gramming.gram(my_tokens, grams=[(0,), (0, 2)])
 
 def test_gramming():
     eq_(solve(my_grams, cache={my_tokens: ["one", "two", "three", "four"]}),
-        ["one", ("one", "three"), "two", ("two", "four"), "three",
-         "four"])
+        [("one",), ("one", "three"), ("two",), ("two", "four"), ("three",),
+         ("four",)])
 
     eq_(pickle.loads(pickle.dumps(my_grams)),
         my_grams)
