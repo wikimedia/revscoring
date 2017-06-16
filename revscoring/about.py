a1cd9eac178d45bee543f46c80e01ab4805ca7d6
halfak <aaron.halfaker@gmail.com>
2017-06-16 16:20:06 -0500

    Increments version to 1.3.14

--- revscoring/about.py
+++ revscoring/about.py
@@ -1,5 +1,5 @@
 __name__ = "revscoring"
-__version__ = "1.3.13"
+__version__ = "1.3.14"
 __author__ = "Aaron Halfaker"
 __author_email__ = "ahalfaker@wikimedia.org"
 __description__ = ("A set of utilities for generating quality scores for " +
