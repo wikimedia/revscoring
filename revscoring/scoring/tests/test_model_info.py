dacb83ccaa19d976a8766d85726046b73c0fd4c7
mdew192837 <justin.d128@gmail.com>
2017-08-10 15:40:14 -0500

    Handle path special cases

--- revscoring/scoring/tests/test_model_info.py
+++ revscoring/scoring/tests/test_model_info.py
@@ -9,5 +9,9 @@ def test_model_info():
     mi['bar'] = 2
     mi['baz'] = 3
 
+    assert 'foo' in mi.format([''], formatting="json")
+    assert 'bar' in mi.format([''], formatting="json")
+    assert 'baz' not in mi.format([''], formatting="json")
+
     eq_(list(mi.keys()), ['foo', 'bar', 'baz'])
     eq_(list(mi.format_json({}).keys()), ['foo', 'bar'])
