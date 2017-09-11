ac949cb27dff8f2414a188dc4bb0b772f7b6665d
Adam Roses Wight <awight@wikimedia.org>
2017-09-11 12:44:16 -0700

    Increments version to 1.3.19

--- revscoring/about.py
+++ revscoring/about.py
@@ -1,5 +1,5 @@
 __name__ = "revscoring"
-__version__ = "1.3.18"
+__version__ = "1.3.19"
 __author__ = "Aaron Halfaker"
 __author_email__ = "ahalfaker@wikimedia.org"
 __description__ = ("A set of utilities for generating quality scores for " +
