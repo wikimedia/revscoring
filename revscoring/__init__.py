2f4e9239a334b581abd7ed28a6e762ba927445ee
Devin/Zppix <megadev44s.mail@gmail.com>
2017-12-01 16:26:58 -0600

    Flake8 fixes pt.1

--- revscoring/__init__.py
+++ revscoring/__init__.py
@@ -138,8 +138,8 @@ from .about import (__author__, __author_email__, __description__, __name__,
 
 
 if sys.version_info <= (3, 0):
-    raise VersionConflict("Revscoring requires Python '>=3' but your Python version is "
-                          + platform.python_version())
+    raise VersionConflict("Revscoring requires Python '>=3' but your Python version is " + 
+                          platform.python_version())
 
 
 __all__ = [Datasource, Dependent, DependentSet, Extractor, Feature,
