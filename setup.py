e8aca088d042dfb00c9ee64cfcbb941b82b32f69
Devin/Zppix <megadev44s.mail@gmail.com>
2017-12-01 17:23:54 -0600

    Syntax fix

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def requirements(fname):
 
 
 setup(
-    python_requires="<=3"
+    python_requires=">=3"
     name=__name__,  # noqa
     version=__version__,  # noqa
     author=__author__,  # noqa
