4d0a0c739e8ceb0130f5ca1e841848e423428386
halfak <aaron.halfaker@gmail.com>
2018-02-13 10:07:00 -0600

    Adds GB multilabel test.

--- revscoring/scoring/models/tests/test_gradient_boosting.py
+++ revscoring/scoring/models/tests/test_gradient_boosting.py
@@ -22,3 +22,23 @@ def test_gradient_boosting():
     }
     model = Model.from_config(config, 'test')
     assert isinstance(model, GradientBoosting)
+
+def test_random_forest_multilabel():
+    model = GradientBoosting(FEATURES, ["A", "B", "C"], multilabel=True)
+    format_info(model)
+    train_test_multilabel(model)
+    reconstructed_model = pickle_and_unpickle(model)
+    train_test_multilabel(reconstructed_model)
+    format_info(model)
+
+    config = {
+        'scorer_models': {
+            'test': {
+                'class': "revscoring.scoring.models.GradientBoosting",
+                'labels': ["A", "B", "C"],
+                'features': [1, 2, 3]
+            }
+        }
+    }
+    model = Model.from_config(config, 'test')
+    assert isinstance(model, GradientBoosting)
