43f152a2e37e90d9eb6d18bb5ff5705171b50025
Sumit Asthana <asthana.sumit23@gmail.com>
2018-02-27 18:36:04 +0530

    Make word vectors accept loaded vectors

--- revscoring/datasources/meta/tests/test_vectorizers.py
+++ revscoring/datasources/meta/tests/test_vectorizers.py
@@ -1,4 +1,3 @@
-from unittest.mock import patch
 import numpy as np
 
 from .. import vectorizers
@@ -10,15 +9,10 @@ test_vectors = {'a': np.ones(200),
                 'b': np.ones(200),
                 'c': np.ones(200)}
 
-loadvectors_full_path = vectorizers.__name__ + \
-    '.word2vec.load_kv'
 
-
-@patch(loadvectors_full_path)
-def test_vectorize(loadw2v):
-    loadw2v.return_value = test_vectors
+def test_vectorize():
     wv = vectorizers.word2vec(wikitext.revision.datasources.tokens,
-                              'prefix', 'vector_name', dim=200)
+                              test_vectors, dim=200, name='word vectors')
     vector = solve(wv, cache={ro.revision.text: 'a bv c d'})
     assert len(vector) == 7
     assert len(vector[0]) == 200
