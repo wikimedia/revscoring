64b4e175d59f3f7c06ef009c0e0ff2cbf07e8e1e
Sumit Asthana <asthana.sumit23@gmail.com>
2018-02-27 19:15:00 +0530

    fix flake8

--- revscoring/datasources/meta/tests/test_vectorizers.py
+++ revscoring/datasources/meta/tests/test_vectorizers.py
@@ -1,13 +1,12 @@
-import numpy as np
 
 from .. import vectorizers
 from revscoring.datasources import revision_oriented as ro
 from revscoring.dependencies import solve
 from revscoring.features import wikitext
 
-test_vectors = {'a': [1]*200,
-                'b': [1]*200,
-                'c': [1]*200}
+test_vectors = {'a': [1] * 200,
+                'b': [1] * 200,
+                'c': [1] * 200}
 
 
 def test_vectorize():
