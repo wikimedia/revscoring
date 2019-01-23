wikimedia/revscoring

commit c1da24de09abc3d68275524caefbbb5666309ad2

Make basic datasources all json-serializable
Bug: T206333

revscoring/extractors/api/extractor.py
@@ -56,7 +56,7 @@ class Extractor(BaseExtractor):
             rev_ids : int | `iterable`
                 Either a single rev_id or an `iterable` of rev_ids
             dependents : :class:`~revscoring.dependents.dependent.Dependent`
-                A set of dependents to extract values for
+                A list of dependents to extract values for
             context : `dict` | `iterable`
                 A set of call-specific
                 :class:`~revscoring.Dependent` to inject
@@ -94,7 +94,7 @@ class Extractor(BaseExtractor):
 
     def _extract_many(self, rev_ids, dependents, context, caches, cache,
                       profile):
-        all_dependents = set(expand(dependents))
+        all_dependents = list(set(expand(dependents)))
 
         caches = caches if caches is not None else {}
         caches.update({rev_id: {} for rev_id in rev_ids
@@ -213,7 +213,7 @@ class Extractor(BaseExtractor):
                     yield e, None
 
     def _extract(self, rev_id, dependents, context, cache, profile):
-        all_dependents = set(expand(dependents))
+        all_dependents = list(set(expand(dependents)))
 
         cache.update({self.revision.id: rev_id,
                       self.dependents: all_dependents})

revscoring/datasources/revision_oriented.py
@@ -55,7 +55,7 @@ class Revision(DependentSet):
         self.id = Datasource(name + ".id")
         "`int` : Revision ID"
         self.timestamp = Datasource(name + ".timestamp")
-        ":class:`mwtypes.Timestamp` : Timestamp the revision was saved"
+        "`str` : Timestamp the revision was saved in ISO format"
         self.comment = Datasource(name + ".comment")
         "`str` : The comment saved with the revision"
         self.byte_len = Datasource(name + ".byte_length")
@@ -159,9 +159,9 @@ class UserInfo(DependentSet):
         self.editcount = Datasource(name + ".editcount")
         "`int` : A count of edits the user has ever saved"
         self.registration = Datasource(name + ".registration")
-        ":class:`mwtypes.Timestamp` : The date the user registered"
+        "`str` : The date the user registered in ISO format"
         self.groups = Datasource(name + ".groups")
-        "`set` ( `str` ) : The groups the user is a member of"
+        "`list` ( `str` ) : The groups the user is a member of"
         self.emailable = Datasource(name + ".emailable")
         "`bool` : `True` if the users is emailable, `False` otherwise"
         self.gender = Datasource(name + ".gender")

revscoring/extractors/api/revision_oriented.py
@@ -1,11 +1,9 @@
 import logging
 
-import mwtypes
-
 from ...datasources import Datasource
 from ...dependencies import DependentSet
 from ...errors import CommentDeleted, TextDeleted, UserDeleted
-from .util import key, key_exists, or_none
+from .util import key, key_exists
 
 logger = logging.getLogger(__name__)
 
@@ -19,8 +17,7 @@ class Revision(DependentSet):
         self.id = id_datasource or key('revid', rev_doc, name=revision.id.name)
 
         self.timestamp = key('timestamp', rev_doc,
-                             name=revision.timestamp.name,
-                             apply=mwtypes.Timestamp)
+                             name=revision.timestamp.name)
         self.comment = key('comment', rev_doc, name=revision.comment.name,
                            if_missing=(CommentDeleted, revision.comment))
         self.byte_len = key('byte_len', rev_doc,
@@ -118,10 +115,8 @@ class RevisionUserInfo(DependentSet):
         self.editcount = key('editcount', self.doc,
                              name=user.info.editcount.name)
         self.registration = key('registration', self.doc,
-                                name=user.info.registration.name,
-                                apply=or_none(mwtypes.Timestamp))
-        self.groups = key('groups', self.doc, name=user.info.groups.name,
-                          apply=set)
+                                name=user.info.registration.name)
+        self.groups = key('groups', self.doc, name=user.info.groups.name)
         self.emailable = key_exists('emailable', self.doc,
                                     name=user.info.emailable.name)
         self.gender = key('gender', self.doc, name=user.info.gender.name)

revscoring/features/temporal/revision_oriented.py
@@ -1,15 +1,13 @@
 import logging
 from datetime import datetime
 
-import mwtypes
-
-from pytz import utc
+from mwtypes import Timestamp
 
 from ...datasources import revision_oriented
 from ...dependencies import DependentSet
 from ..feature import Feature
 
-MW_REGISTRATION_EPOCH = mwtypes.Timestamp("2006-01-01T00:00:00Z")
+MW_REGISTRATION_EPOCH = '2006-01-01T00:00:00Z'
 
 logger = logging.getLogger(__name__)
 
@@ -164,14 +162,15 @@ class PageCreation(DependentSet):
 def _process_day_of_week(timestamp):
     if timestamp is None:
         return 7  # The day after Sunday.
-    dt = datetime.fromtimestamp(timestamp.unix(), tz=utc)
+    # TODO: After release of python3.7 use fromisoformat instead
+    dt = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
     return dt.weekday()
 
 
 def _process_hour_of_day(timestamp):
     if timestamp is None:
         return 24  # The hour after midnight
-    dt = datetime.fromtimestamp(timestamp.unix(), tz=utc)
+    dt = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
     return dt.hour
 
 
@@ -179,7 +178,7 @@ def _process_seconds_since(old_timestamp, current_timestamp):
     if old_timestamp is None:
         return 0
     else:
-        return current_timestamp - old_timestamp
+        return Timestamp(current_timestamp) - Timestamp(old_timestamp)
 
 
 def _process_seconds_since_registration(id, registration, timestamp):
@@ -188,7 +187,8 @@ def _process_seconds_since_registration(id, registration, timestamp):
     else:
         # Handles users who registered before registration dates were
         # recorded
-        registration = registration or MW_REGISTRATION_EPOCH
+        registration = Timestamp(registration or MW_REGISTRATION_EPOCH)
+        timestamp = Timestamp(timestamp)
         if registration > timestamp:
             # Something is weird.  Probably an old user.
             logger.info("Timestamp chronology issue {0} < {1}"

revscoring/features/temporal/tests/test_revision_oriented.py
@@ -1,20 +1,18 @@
 import pickle
 
-from mwtypes import Timestamp
-
 from ....datasources import revision_oriented
 from ....dependencies import solve
-from ..revision_oriented import MW_REGISTRATION_EPOCH, revision
+from ..revision_oriented import revision
 
 
 def test_revision():
 
-    cache = {revision_oriented.revision.timestamp: Timestamp(0)}
+    cache = {revision_oriented.revision.timestamp: '1970-01-01T00:00:00Z'}
     assert solve(
         revision.day_of_week,
         cache=cache) == 3  # Thursday, Jan 1 1970
 
-    cache = {revision_oriented.revision.timestamp: Timestamp(0)}
+    cache = {revision_oriented.revision.timestamp: '1970-01-01T00:00:00Z'}
     assert solve(revision.hour_of_day, cache=cache) == 0  # Midnight
 
     assert pickle.loads(pickle.dumps(revision.day_of_week)
@@ -26,8 +24,8 @@ def test_revision():
 def test_page_creation():
 
     cache = {
-        revision_oriented.revision.timestamp: Timestamp(10),
-        revision_oriented.revision.page.creation.timestamp: Timestamp(0)
+        revision_oriented.revision.timestamp: '1970-01-01T00:00:10Z',
+        revision_oriented.revision.page.creation.timestamp: '1970-01-01T00:00:00Z'
     }
     assert solve(revision.page.creation.seconds_since, cache=cache) == 10
 
@@ -38,15 +36,15 @@ def test_page_creation():
 def test_user_registration():
 
     cache = {
-        revision_oriented.revision.timestamp: Timestamp(10),
+        revision_oriented.revision.timestamp: '1970-01-01T00:00:10Z',
         revision_oriented.revision.user.id: 10,
-        revision_oriented.revision.user.info.registration: Timestamp(0)
+        revision_oriented.revision.user.info.registration: '1970-01-01T00:00:00Z'
     }
     assert solve(revision.user.seconds_since_registration, cache=cache) == 10
 
     # Anon (no registration)
     cache = {
-        revision_oriented.revision.timestamp: Timestamp(10),
+        revision_oriented.revision.timestamp: '1970-01-01T00:00:00Z',
         revision_oriented.revision.user.id: 0,
         revision_oriented.revision.user.info.registration: None
     }
@@ -54,7 +52,7 @@ def test_user_registration():
 
     # Old user (no registration)
     cache = {
-        revision_oriented.revision.timestamp: MW_REGISTRATION_EPOCH + 10,
+        revision_oriented.revision.timestamp: '2006-01-01T00:00:10Z',
         revision_oriented.revision.user.id: 10,
         revision_oriented.revision.user.info.registration: None
     }
@@ -62,9 +60,9 @@ def test_user_registration():
 
     # Old user (broken registration date)
     cache = {
-        revision_oriented.revision.timestamp: Timestamp(0),
+        revision_oriented.revision.timestamp: '1970-01-01T00:00:00Z',
         revision_oriented.revision.user.id: 10,
-        revision_oriented.revision.user.info.registration: Timestamp(10)
+        revision_oriented.revision.user.info.registration: '1970-01-01T00:00:10Z'
     }
     assert (solve(revision.user.seconds_since_registration, cache=cache) ==
             60 * 60 * 24 * 365)  # one year
@@ -76,13 +74,13 @@ def test_user_registration():
 def test_last_user_revision():
 
     cache = {
-        revision_oriented.revision.timestamp: Timestamp(10),
-        revision_oriented.revision.user.last_revision.timestamp: Timestamp(0)
+        revision_oriented.revision.timestamp: '1970-01-01T00:00:10Z',
+        revision_oriented.revision.user.last_revision.timestamp: '1970-01-01T00:00:00Z'
     }
     assert solve(revision.user.last_revision.seconds_since, cache=cache) == 10
 
     cache = {
-        revision_oriented.revision.timestamp: Timestamp(10),
+        revision_oriented.revision.timestamp: '1970-01-01T00:00:10Z',
         revision_oriented.revision.user.last_revision.timestamp: None
     }
     assert solve(revision.user.last_revision.seconds_since, cache=cache) == 0
@@ -91,8 +89,8 @@ def test_last_user_revision():
 def test_parent_revision():
 
     cache = {
-        revision_oriented.revision.timestamp: Timestamp(10),
-        revision_oriented.revision.parent.timestamp: Timestamp(0)
+        revision_oriented.revision.timestamp: '1970-01-01T00:00:10Z',
+        revision_oriented.revision.parent.timestamp: '1970-01-01T00:00:00Z'
     }
     assert solve(revision.parent.seconds_since, cache=cache) == 10
 

commit a4a536963ab6ed06281e207658f86443377e2357

api extractor: Don't try to "set" & "list"
TypeError: unsupported operand type(s) for &: 'set' and 'list'

Caused by c1da24de09abc3d68275524caefbbb5666309ad2

revscoring/extractors/api/extractor.py
@@ -107,7 +107,7 @@ class Extractor(BaseExtractor):
         errored = {}
 
         # Build up caches for data that can be queried in batch
-        if self.revision & all_dependents:
+        if self.revision and all_dependents:
             rvprop = set(REV_PROPS)
             if self.revision.text in all_dependents:
                 rvprop.add('content')
@@ -136,7 +136,7 @@ class Extractor(BaseExtractor):
                                                        lookup_rev_id)
 
             # datasource.revision.parent.doc
-            if self.revision.parent & all_dependents:
+            if self.revision.parent and all_dependents:
                 parentids_to_lookup = []
                 for rev_id, rev_cache in caches.items():
                     if self.revision.doc in rev_cache and \
@@ -170,7 +170,7 @@ class Extractor(BaseExtractor):
                                 RevisionNotFound(self.revision.parent,
                                                  parent_id)
 
-            if self.revision.user.info & all_dependents:
+            if self.revision.user.info and all_dependents:
                 user_texts_to_lookup = set()
                 for rev_id, rev_cache in caches.items():
                     if self.revision.doc in rev_cache and \