6c4fdc4a35bfe58216f7a5e1c189471ee0b0ecb9
Habeeb Shopeju <shopejuh@gmail.com>
2020-03-10 23:50:15 +0100

    Modify revscoring.utilities.fetch_idioms.fetch method (#477)
    Modify revscoring.assets.enwiktionary_idioms file
    
    The idioms gotten from Enwikitionary Category:English idioms,
    have problematic idioms, with many of them being regular
    English phrases instead of idioms. The list was also quite
    long, causing it to take a long time when matching idioms.
    Idioms can be problematic in formal articles.
    Hence, as many regular phrases as possible have to be removed, and
    the list should be reduced too.
    
    Task: T247000

--- revscoring/utilities/fetch_idioms.py
+++ revscoring/utilities/fetch_idioms.py
@@ -17,6 +17,7 @@
 """
 
 import logging
+import re
 import sys
 
 import docopt
@@ -44,7 +45,69 @@ def main(argv=None):
     run(output, verbose)
 
 
-def fetch(output):
+def is_idiom(phrase):
+    """
+        Checks if a phrase meets certain criteria to make it an idiom
+    """
+    if re.match('Category:English|Citation:|Appendix:', phrase):
+        return False
+    # One word phrases
+    if re.match(r"^[\w\-\']+$", phrase):
+        return False
+    # Two-worded phrases
+    if re.match(r"^[\w\-\']+ [\w\-\']+$", phrase):
+        return False
+    # Similes
+    if 'as a' in phrase:
+        return False
+    return True
+
+
+def combine_words(first_word, following_words):
+    """
+        Combines the first word and following words into one regex
+        Sample:
+        first_word (word1|word2|...|wordx)
+    """
+    if len(following_words) == 1:
+        idiom_regex = '{} {}'.format(
+            first_word, "".join(following_words))
+    else:
+        idiom_regex = '{} ({})'.format(
+            first_word, "|".join(following_words))
+    if not idiom_regex.isspace():
+        return idiom_regex
+
+
+def create_regexes(idioms):
+    """
+        Creates regexes out of idioms
+    """
+    regexes = []
+    first_word = ''
+    following_words = []
+    for idiom in idioms:
+        words = idiom.split()
+        if first_word == '':
+            pass
+        elif first_word == words[0]:
+            following_words.append(" ".join(words[1:]))
+            continue
+        else:
+            regex = combine_words(first_word, following_words)
+            if regex:
+                regexes.append(regex)
+        first_word = words[0]
+        following_words = [" ".join(words[1:])]
+
+    regex = combine_words(first_word, following_words)
+    if regex:
+        regexes.append(regex)
+
+    return regexes
+
+
+def fetch():
     session = mwapi.Session("https://en.wiktionary.org")
 
     results = session.get(
@@ -57,20 +120,20 @@ def fetch(output):
     idioms = []
     for doc in results:
         for page_doc in doc['query']['categorymembers']:
-            # Some values take the form:
-            # Category:English <variable>
-            # For example: Category:English rhetorical questions
-            # They are not idioms and should be filtered
-            idiom = page_doc['title']
-            if 'Category:English' not in idiom:
-                idioms.append(idiom)
+            phrase = page_doc['title']
+            if not is_idiom(phrase):
+                continue
+            idioms.append(phrase)
 
-    for idiom in idioms:
-        dump_observation(idiom, output)
+    regexes = create_regexes(idioms)
+    return regexes
 
 
 def run(output, verbose):
     logger = logging.getLogger(__name__)
     if verbose:
         logger.info('Fetching idioms...')
-    fetch(output)
+
+    regexes = fetch()
+    for regex in regexes:
+        dump_observation(regex, output)
