21b08aeee8715eb1f7dde69b0b8f7884db9f7b49
5uperpalo <palusko@gmail.com>
2020-11-19 18:01:30 +0100

    flake8 fixed again in tests

--- tests/features/wikitext/tests/test_tokenized.py
+++ tests/features/wikitext/tests/test_tokenized.py
@@ -204,17 +204,20 @@ def test_diff():
     assert (pickle.loads(pickle.dumps(diff.number_prop_delta_decrease)) ==
             diff.number_prop_delta_decrease)
 
+
 def test_cjk_cjks():
     assert (solve(revision.datasources.cjk.cjks, cache={r_text: text}) ==
             ['修造', 'の', 'ため', 'の', '勧進', 'を', '担っ', 'た', '組織', 'の', '総称'])
     assert pickle.loads(pickle.dumps(revision.cjks)) == revision.cjks
-    
+
+
 def test_cjk_tokens():
-    assert (solve(revision.datasources.cjk.tokens, cache={r_text: text}) == ['\n', 'This', ' ', 'is', ' ',
-            'an', ' ', 'm80', '.', '  ', 'It', ' ', 'has', ' ', '50', ' ', 'grams', ' ', 'of', ' ', 'TNT',
-            '.', ' ', "Here's", ' ', 'some', ' ', 'japanese', ':', '\n', '修造', 'の', 'ため', 'の', '勧進',
-            'を', '担っ', 'た', '組織', 'の', '総称', '。', '[', '//google.com?foo=bar', ' ', 'hats', ']', '\n',
-            'I', ' ', 'can', ' ', 'use', ' ', '&middot;', ' ', 'and', ' ', '&nbsp;', '.', '  ', 'But', ' ',
-            '[[', 'can', ']]', ' ', 'I', ' ', '{{', 'foo', '}}', ' ', 'a', ' ', '{{', 'bar', '}}', '?', '\n\n',
-            'I', ' ', 'guess', ' ', "we'll", ' ', 'never', ' ', 'know', '.', '\n'])
+    assert (solve(revision.datasources.cjk.tokens, cache={r_text: text}) == [
+            '\n', 'This', ' ', 'is', ' ', 'an', ' ', 'm80', '.', '  ', 'It', ' ', 'has', ' ', '50', ' ',
+            'grams', ' ', 'of', ' ', 'TNT', '.', ' ', "Here's", ' ', 'some', ' ', 'japanese', ':', '\n',
+            '修造', 'の', 'ため', 'の', '勧進', 'を', '担っ', 'た', '組織', 'の', '総称', '。', '[',
+            '//google.com?foo=bar', ' ', 'hats', ']', '\n', 'I', ' ', 'can', ' ', 'use', ' ', '&middot;',
+            ' ', 'and', ' ', '&nbsp;', '.', '  ', 'But', ' ', '[[', 'can', ']]', ' ', 'I', ' ', '{{', 'foo',
+            '}}', ' ', 'a', ' ', '{{', 'bar', '}}', '?', '\n\n', 'I', ' ', 'guess', ' ', "we'll", ' ',
+            'never', ' ', 'know', '.', '\n'])
     assert pickle.loads(pickle.dumps(revision.cjks)) == revision.cjks
