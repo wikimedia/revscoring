f7d489f9e71ee2f95317af416069ebc4b3c74b22
Luca Toscano <ltoscano@wikimedia.org>
2022-12-16 16:50:56 +0100

    Update numpy and nltk dependencies and release 2.11.7
    The GH's dependabot alerted us about some dependencies that need
    a bump to fix some low priority security concerns.
    
    Release also version 2.11.7
    
    Bug: T325366

--- revscoring/about.py
+++ revscoring/about.py
@@ -1,5 +1,5 @@
 __name__ = "revscoring"
-__version__ = "2.11.6"
+__version__ = "2.11.7"
 __author__ = "Aaron Halfaker"
 __author_email__ = "ahalfaker@wikimedia.org"
 __description__ = ("A set of utilities for generating quality scores for " +
