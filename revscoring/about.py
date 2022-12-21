5103b783df2ba10546701f863b5317234a41330a
isaranto <isarantopoulos@wikimedia.org>
2022-12-21 12:54:04 +0200

    Push new version

--- revscoring/about.py
+++ revscoring/about.py
@@ -1,5 +1,5 @@
 __name__ = "revscoring-isaranto-test"
-__version__ = "2.5.7"
+__version__ = "2.5.8"
 __author__ = "Aaron Halfaker"
 __author_email__ = "ahalfaker@wikimedia.org"
 __description__ = ("A set of utilities for generating quality scores for " +
